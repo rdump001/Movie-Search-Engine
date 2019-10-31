78f7f5d30dd10a46de7bdacddec490e630f8ec8d
Rahul Dumpala <47930384+rdump001@users.noreply.github.com>
2019-10-31 11:47:24 -0400

    milestonedump2.sql

--- milestone1dump.sql
+++ milestone1dump.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 08, 2019 at 07:46 AM
+-- Generation Time: Oct 31, 2019 at 04:46 PM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.1.32
 
@@ -25,38 +25,42 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table `user`
 --
 
-CREATE TABLE `users` (
+CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `password` varchar(100) NOT NULL,
   `username` varchar(100) NOT NULL,
   `emailId` varchar(100) NOT NULL,
   `age` int(255) DEFAULT NULL,
   `country` varchar(255) DEFAULT NULL,
-  `gender` varchar(10) DEFAULT NULL
+  `gender` varchar(10) DEFAULT NULL,
+  `activation` char(32) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `activation2` char(32) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `users`
+-- Dumping data for table `user`
 --
 
-INSERT INTO `users` (`id`, `password`, `username`, `emailId`, `age`, `country`, `gender`) VALUES
-(6, 'Alex@1234', 'alex', 'alex@gmail.com', 24, 'USA', 'Female'),
-(7, 'Sid@1234', 'Sid', 'sid@outlook.com', NULL, NULL, NULL),
-(8, 'John@1234', 'John', 'john@gmail.com', NULL, NULL, NULL),
-(9, 'Sam@1234', 'Sam', 'sam@gmail.com', NULL, NULL, NULL),
-(10, 'Stark@1234', 'Stark', 'stark@gmail.com', 23, 'USA', 'Male');
+INSERT INTO `user` (`id`, `password`, `username`, `emailId`, `age`, `country`, `gender`, `activation`, `activation2`) VALUES
+(6, 'Alex@4321', 'alex', 'alex@gmail.com', 24, 'USA', 'Female', '', NULL),
+(7, 'Sid@1234', 'Sid', 'sid@outlook.com', NULL, NULL, NULL, '', NULL),
+(8, 'John@1234', 'John', 'john@gmail.com', NULL, NULL, NULL, '', NULL),
+(9, 'Sam@1234', 'Sam', 'sam@gmail.com', NULL, NULL, NULL, '', NULL),
+(10, 'Stark@1234', 'Stark', 'stark@gmail.com', 23, 'USA', 'Male', '', NULL),
+(72, 'Shreya@1234', 'Shreya', 'shreyalibra22@gmail.com', NULL, NULL, NULL, 'f4dce00d93d895bcb246a17981661997', NULL),
+(73, 'Abhi@1234', 'Abhilash', 'abhi158.aa@gmail.com', NULL, NULL, NULL, '3ef5404ffa3d9fc2a6ca07afcc936791', NULL);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `users`
+-- Indexes for table `user`
 --
-ALTER TABLE `users`
+ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id` (`id`),
   ADD UNIQUE KEY `username` (`username`);
@@ -66,10 +70,10 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT for table `user`
 --
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
